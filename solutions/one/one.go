05b9808a075572ad4a7fcf0c62eba7630549e63f
Matt Olson <matt@mattolson.com>
2013-09-23 15:57:27 -0700

    Solution for problem 1

--- solutions/one/one.go
+++ solutions/one/one.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Printf("hello, world\n")
+  sum := 0
+  for i := 0; i < 1000; i++ {
+    if i % 3 == 0 || i % 5 == 0 {
+      sum += i
+    }
+  }
+  fmt.Println(sum)
 }
 
